879c7467aac9579ead5a486441a92d891f65c33b
Yanuar Aristya <yanuararistya@gmail.com>
2019-10-03 20:53:54 +0900

    initialize repo, init menu scene

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScene : MonoBehaviour
+{
+    InputField _nameField;
+    Button _joinButton;
+
+    void Awake ()
+    {
+        _nameField = GetComponentInChildren<InputField>();
+        _joinButton = GetComponentInChildren<Button>();
+    }
+
+    void Start ()
+    {
+        _nameField.onValueChanged.AddListener(delegate{SetButtonInteractability();});
+    }
+
+    void SetButtonInteractability ()
+    {
+        _joinButton.interactable  = _nameField.text != "";
+    }
+}
