cc5a0010e30d20378ddda3ff31f42dd1e150dac4
Yanuar Aristya <yanuararistya@gmail.com>
2019-10-03 23:22:00 +0900

    added lobby id inputfield, created lobby scene

--- Assets/Scripts/MenuScene.cs
+++ Assets/Scripts/MenuScene.cs
@@ -3,22 +3,48 @@ using UnityEngine.UI;
 
 public class MenuScene : MonoBehaviour
 {
-    InputField _nameField;
-    Button _joinButton;
+    #region Constants
+    const int MAX_LOBBY_ID = 9999;
+    #endregion
 
-    void Awake ()
-    {
-        _nameField = GetComponentInChildren<InputField>();
-        _joinButton = GetComponentInChildren<Button>();
-    }
+    #region SerializeFields
+    [SerializeField] InputField _nameField = null;
+    [SerializeField] InputField _lobbyField = null;
+    [SerializeField] Button _joinButton = null;
+    [SerializeField] Button _randomizeButton = null;
+    #endregion
 
+    #region PrivateVariables
+    int _lobbyIdentifier = 0;
+    #endregion
+
+    #region UnityLifecycles
     void Start ()
     {
         _nameField.onValueChanged.AddListener(delegate{SetButtonInteractability();});
-    }
 
+        _lobbyField.onValueChanged.AddListener(delegate{SetButtonInteractability();});
+        _lobbyField.onValueChanged.AddListener(delegate{SetLobbyIdentifier();});
+
+        _randomizeButton.onClick.AddListener(delegate{RandomizeLobbyIdentifier();});
+    }
+    #endregion
+
+    #region PrivateMethods
     void SetButtonInteractability ()
     {
-        _joinButton.interactable  = _nameField.text != "";
+        _joinButton.interactable  = _nameField.text != "" && _lobbyField.text != "";
     }
+
+    void SetLobbyIdentifier ()
+    {
+        System.Int32.TryParse(_lobbyField.text, out _lobbyIdentifier);
+    }
+
+    void RandomizeLobbyIdentifier ()
+    {
+        _lobbyIdentifier = Random.Range(0, MAX_LOBBY_ID);
+        _lobbyField.text = _lobbyIdentifier.ToString();
+    }
+    #endregion
 }
